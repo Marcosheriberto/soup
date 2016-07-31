15e3f8b5a1150130c4a4d127b85fcdf9ed4b8f9f
Hugo Hernani <hugo@quezmedia.com>
2016-07-31 16:00:54 -0300

    Instagram OmniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :instagram, ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET']
 end
